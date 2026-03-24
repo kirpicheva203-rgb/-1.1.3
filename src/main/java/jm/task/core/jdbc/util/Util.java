6523a2af1e7990c51cadf0d45b5c7bb16d02f16a
kirpicheva203-rgb <kirpicheva203@gmail.com>
2026-03-24 18:21:19 +0300

    Add files via upload

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,17 +1,30 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.SessionFactory;
+
+import org.hibernate.cfg.Configuration;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
+
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+
+import org.hibernate.service.ServiceRegistry;
 
 public class Util {
 
-        private static final String URL = "jdbc:mysql://localhost:3306/mydb";
-        private static final String USERNAME = "root";
-        private static final String PASSWORD = "Atw77kl924!";
+    private static final String URL = "jdbc:mysql://localhost:3306/mydb";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Atw77kl924!";
+    private static Connection connection = null;
+    private static SessionFactory factory;
 
     public static Connection getConnection() {
-        Connection connection = null;
         try {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
@@ -19,5 +32,13 @@ public class Util {
         }
         return connection;
     }
-
+    public static void closeConnection() {
+        if (connection != null){
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
